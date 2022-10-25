d51b8830171d8da3525165affe25e513db843bd8
aravind <aravi@LAPTOP-5LCMP4L4>
2022-10-25 19:20:04 +0530

    added sub category and category module

---
+++ BackEnd/ExpenseModule/src/main/java/com/expensemanager/controller/ExpenseController.java
@@ -0,0 +1,20 @@
+package com.expensemanager.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+
+	private Logger logger = LoggerFactory.getLogger(ExpenseController.class);
+	
+	
+	
+	@GetMapping(path = "/expense")
+	public String sayHello() {
+		return "Hello";
+	}
+}
